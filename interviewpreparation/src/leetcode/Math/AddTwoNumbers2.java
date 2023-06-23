817a9fc93c6d78a6d3450e7c6df76f6a6d05bbbe
Deepak M <deepak.a.malhotra@capgemini.com>
2023-06-23 13:08:23 -0500

    Test file

---
+++ interviewpreparation/src/leetcode/Math/AddTwoNumbers2.java
@@ -0,0 +1,14 @@
+package leetcode.Math;
+
+public class AddTwoNumbers2 {
+
+	public AddTwoNumbers2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
