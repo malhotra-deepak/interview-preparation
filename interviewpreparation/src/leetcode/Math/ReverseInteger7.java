fdbf6a81f4ddc5a05f0898328a199d2c32f1edb3
Deepak M <deepak.a.malhotra@capgemini.com>
2023-06-23 17:18:48 -0500

    Add ReverseInteger7.java

---
+++ interviewpreparation/src/leetcode/Math/ReverseInteger7.java
@@ -0,0 +1,15 @@
+package leetcode.Math;
+
+public class ReverseInteger7 {
+
+	public ReverseInteger7() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This program is to reverse an integer in Java for testing");
+
+	}
+
+}
